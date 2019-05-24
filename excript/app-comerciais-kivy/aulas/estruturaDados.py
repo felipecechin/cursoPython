c2824aa49bb6b108a2f55c7451cc96da0cdb53cd
Felipe <ficechin@hotmail.com>
2019-05-23 23:53:52 -0300

    Algumas seções concluídas.

---
+++ excript/app-comerciais-kivy/aulas/estruturaDados.py
@@ -0,0 +1,17 @@
+#lista tem colchetes
+lista = [1,2,3,4,5,6,7]
+lista = ["fa", 3, "a"]
+print(lista[1:])
+
+print(lista[0])
+
+lista = list("eXcript")
+print(lista[-1])
+
+#tupla tem parenteses - é igual a lista só que imutável
+tupla = (1,2,3)
+print(tupla[0])
+
+#dicionario tem chaves
+dicionario = {"aa": "oi", 2:50}
+print(dicionario["aa"])
---
+++ excript/app-comerciais-kivy/aulas/iteracoes.py
@@ -0,0 +1,12 @@
+x= 0
+while (x<10):
+    print(x)
+    x=x+1
+else:
+    print("else")
+
+for c in "eXcript":
+    print(c)
+
+for i in range(10):
+    print(i)
