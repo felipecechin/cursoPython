194932b0e93947ba2ba17bdbcdf5dc11de98f195
Felipe <ficechin@hotmail.com>
2019-06-04 00:21:16 -0300

    Empacotamento Android com KVLauncher.

---
+++ kivy/source/empacotamento/ola-mundo/main.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require("1.9.1")
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class OlaMundoApp(App):
+    def build(self):
+        return Button(text="Funcionou",
+                      size_hint=(.2,.2),
+                      pos_hint={"center_x": .5, "center_y": .5}
+                      )
+
+if __name__ == '__main__':
+    OlaMundoApp().run()
